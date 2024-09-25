04283d2b622ff24ea73074bdca6f61341e6cbdd9
Kiran Surya <146125805+KiranSurya-SU@users.noreply.github.com>
2024-09-25 22:27:23 +0530

    Add files via upload

---
+++ JavaScript/pro1.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 30;
+var c = 40;
+
+if (a>b && a>c){
+    console.log("A is greater");
+}
+else if (b>a && b>c){
+    console.log("B is greater");
+}
+else{
+    console.log("C is greater");
+}
